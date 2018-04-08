ba4f95d9fe9a2a2ab0a031f04ccf7f9ecfc3920d
Tony0215 <ol3577600@yahoo.com.tw>
2018-04-09 01:01:25 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@
 #include <ctime>
 #include <cstdlib>
 #include <ctime>
-// 10227150 ¥Õµq¤¸ 10227135 ªô¥@øÊ 
+
 using namespace std;
 
 void print_board(vector <int >);
@@ -18,44 +18,44 @@ int main()
    int command = -1, num = -1, index = -1, get = -1, q = -1;
    Game start ;
    Game_nine start_two ;
-   SudokuGen NewBoard; // ²£¥Í´Ñ½Lª«¥ó
+   SudokuGen NewBoard; // ç”¢ç”Ÿæ£‹ç›¤ç‰©ä»¶
    SudokuGen_nine NewBoard_two;
    vector <int > board;
    vector <int > boardtwo;
    bool out = true ;
    clock_t start1, stop;
    do { 
-   cout << endl << "***** «ü¥O¤¶­± ********************";
-   cout << endl << "* 0. QUIT µ²§ô                    *";
-   cout << endl << "* 1. Manage. ºŞ²z                 *";
-   cout << endl << "* 2. Play. ¹CÀ¸                   *";
-   cout << endl << "* 3. ¯S®í     9®c®æ¹CÀ¸           *";
+   cout << endl << "***** æŒ‡ä»¤ä»‹é¢ ********************";
+   cout << endl << "* 0. QUIT çµæŸ                    *";
+   cout << endl << "* 1. Manage. ç®¡ç†                 *";
+   cout << endl << "* 2. Play. éŠæˆ²                   *";
+   cout << endl << "* 3. ç‰¹æ®Š     9å®®æ ¼éŠæˆ²           *";
    cout << endl << "***********************************";
    cout << endl << "Input a command(0, 1, 2, 3): ";
-   cin >> command; //¹Á¸ÕÅª¨ú¾Ş§@«ü¥O
+   cin >> command; //å˜—è©¦è®€å–æ“ä½œæŒ‡ä»¤
    switch (command)
    { case 0: return 0 ;
      case 1: //
 	         do {
-               cout << "(ºŞ²z)" << endl << "0. ·s¼WÃD«¬" << endl << "1. Åã¥ÜÃD«¬" << endl << "2. §R°£ÃD«¬" << endl << "3. µ²§ôºŞ²z" << endl ;
+               cout << "(ç®¡ç†)" << endl << "0. æ–°å¢é¡Œå‹" << endl << "1. é¡¯ç¤ºé¡Œå‹" << endl << "2. åˆªé™¤é¡Œå‹" << endl << "3. çµæŸç®¡ç†" << endl ;
                cin >> command;
                switch (command) {
                	  case 0:
                	  	          NewBoard.generator();
-                              board = NewBoard.get_board(); //¨ú±o´Ñ½L
-                              print_board(board); // ±N´Ñ½L¦L¥X
-                              cout << "[·s¼WÃD«¬§¹¦¨!!]" << endl ;
+                              board = NewBoard.get_board(); //å–å¾—æ£‹ç›¤
+                              print_board(board); // å°‡æ£‹ç›¤å°å‡º
+                              cout << "[æ–°å¢é¡Œå‹å®Œæˆ!!]" << endl ;
                	  	break;
                	  case 1:
                	  	          for ( int q = 0 ; q < NewBoard.All_board.size(); q++ ) 
 								cout << "[ " << q << " ]" <<endl ,print_board(NewBoard.All_board[q]),
 								print_board(NewBoard.All_board_ANS[q]) ;
-							  cout << "[Åã¥ÜÃD«¬§¹¦¨!!]" << endl ;	
+							  cout << "[é¡¯ç¤ºé¡Œå‹å®Œæˆ!!]" << endl ;	
                	  	break;
-				  case 2:     cout << "½Ğ¿é¤J­n§R°£ÃD«¬ªº¸¹½X: " , cin >> num ;
+				  case 2:     cout << "è«‹è¼¸å…¥è¦åˆªé™¤é¡Œå‹çš„è™Ÿç¢¼: " , cin >> num ;
 				  	          if ( num < NewBoard.All_board.size() ) 
 								NewBoard.All_board_ANS.erase( NewBoard.All_board_ANS.begin() + num ),  
-								NewBoard.All_board.erase( NewBoard.All_board.begin() + num ), cout << "[§R°£ÃD«¬§¹¦¨!!]" << endl ;
+								NewBoard.All_board.erase( NewBoard.All_board.begin() + num ), cout << "[åˆªé™¤é¡Œå‹å®Œæˆ!!]" << endl ;
 							  else  cout << "Number does not exist!" << endl ;
 							    	   
 				  	break;
@@ -70,25 +70,25 @@ int main()
              break;
      case 2: // 
 	         do {
-               cout << "(ºŞ²z)" << endl << "0. ½m²ß" << endl << "1. ¶}©lÂôÃö" << endl << "2. Æ[¬İ±o¤À" << endl << "3. µ²§ô¹CÀ¸" << endl ;
+               cout << "(ç®¡ç†)" << endl << "0. ç·´ç¿’" << endl << "1. é–‹å§‹é—–é—œ" << endl << "2. è§€çœ‹å¾—åˆ†" << endl << "3. çµæŸéŠæˆ²" << endl ;
                cin >> command;
                switch (command) {
                	  case 0:
                	  	          index =  rand() % (NewBoard.All_board.size()-1) ;
 							  start.test( NewBoard.All_board[index], NewBoard.All_board_ANS[index] ) ; 
                	  	break;
-               	  case 1:     start1 = clock(); //¶}©l®É¶¡ 
+               	  case 1:     start1 = clock(); //é–‹å§‹æ™‚é–“ 
                               for ( int j = 0; j < 3; j++ ) {
 				           	    index =  rand() % (NewBoard.All_board.size()-1) ;
 							    start.test( NewBoard.All_board[index], NewBoard.All_board_ANS[index] ) ;
-							    if ( j == 2 ) cout << "µ²§ô" << endl,stop = clock(),cout << double(stop - start1) / CLOCKS_PER_SEC<< "¬í" <<endl,
+							    if ( j == 2 ) cout << "çµæŸ" << endl,stop = clock(),cout << double(stop - start1) / CLOCKS_PER_SEC<< "ç§’" <<endl,
 								              system("pause"), q = double(stop - start1) / CLOCKS_PER_SEC, start.score.push_back( q ) ; 
-								else cout << "¶}©l¤U¤@ÃD" << endl, system("pause");
+								else cout << "é–‹å§‹ä¸‹ä¸€é¡Œ" << endl, system("pause");
 						      } 
                	  	break;
 				  case 2:     
 				              for ( int j = 0; j < start.score.size() ; j++ )
-				              	cout << "[" << j << "] " << start.score[j] << "¬í" << endl ; 	   
+				              	cout << "[" << j << "] " << start.score[j] << "ç§’" << endl ; 	   
 				  	break;
 				  case 3:
 				             out = false ; 
@@ -101,19 +101,19 @@ int main()
              break;
      case 3: 
 	        NewBoard_two.generator();
-	        boardtwo = NewBoard_two.get_board(); //¨ú±o´Ñ½L
+	        boardtwo = NewBoard_two.get_board(); //å–å¾—æ£‹ç›¤
 	        start_two.test( NewBoard_two.All_board[NewBoard_two.All_board.size()-1], NewBoard_two.All_board_ANS[NewBoard_two.All_board_ANS.size()-1] ) ;
 	        break ;
      default: cout << endl << "Command does not exist!" << endl;
    } // end switch
- } while (true); //«ü¥O'0': µ²§ô¥Dµ{¦¡
+ } while (true); //æŒ‡ä»¤'0': çµæŸä¸»ç¨‹å¼
 	
 
     system("pause");
     return 0;
 }
 
-void print_board(vector <int > board) //±N´Ñ½L¦L¥X
+void print_board(vector <int > board) //å°‡æ£‹ç›¤å°å‡º
 {
     for (int i=0;i<board.size();i++)
     {
